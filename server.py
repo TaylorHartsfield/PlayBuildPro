408978faaed4f48369a5878eebdc4d9f5cb3aeb5
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-07-21 22:12:51 -0600

    MVP Route Setup Stage 1

--- server.py
+++ server.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def homepage():
     return render_template('base.html')
 
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/register_user')
+def register_user():
+    pass
+
+@app.route('/register_show')
+def register_show():
+    pass
+
 if __name__ == "__main__":
     app.run(debug=True)
