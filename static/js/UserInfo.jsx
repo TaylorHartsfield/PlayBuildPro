89e29f8c4f87015c12f2dd3987db83606695a94c
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-08-30 20:50:08 -0600

    Clean up edit user name view

--- static/js/UserInfo.jsx
+++ static/js/UserInfo.jsx
@@ -73,12 +73,30 @@ function ShowUserInfo() {
   
     function renderEditUserInfo(){
         return (
-            <div className="card">
+       <React.Fragment>
+            <div className="userStats">
+                <div className="marqueetext">
+                    <div className="row">
+                        <h2 style={{fontFamily: "showtime", fontSize:"65px"}}>Starring</h2>
+                    </div>
+                    <div className="row" style={{paddingTop: "50px"}}>
+                    <div className="card">
                 <input type="text" placeholder={user.fname} value={user.fname} onChange={handleFNameChange}/>
                 <input type="text" placeholder={user.lname} value={user.lname} onChange={handleLNameChange}/>
                 <button type="submit" onClick={handleSumbission}>Submit your Changes!</button>
             </div>
-        )
+                    </div>
+                
+                </div>
+            </div>
+            <div className="row">
+                <button type="button" onClick={handleOnClick}>Edit User Info</button>
+            </div>
+            </React.Fragment>
+         
+            )
+    
+
     }
     
     function renderUserInfo() {
